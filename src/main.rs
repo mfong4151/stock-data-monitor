83e15aec65caa67469fa697c789176bf61e4887c
Max Fong <mfong415@gmail.com>
2025-03-17 19:11:21 -0700

    Minimum project to push to render

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-
 mod aws_ses;
 mod polygon_api;
 
 use std::io::Error;
-
-use crate::aws_ses::send_email::send_email;
+use axum::{
+  routing::{get},
+  Router
+};
+use std::env;
+async fn test() -> &'static str {
+  "Your server works"
+}
 
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
 
-  let sent_email = send_email().await;
+  let app = Router::new().route("/", get(test ));
+  let port =  env::var("PORT")
+                            .unwrap_or_else(|_| "5000".to_string())
+                            .parse::<u16>()
+                            .expect("PORT must be a number");
+
+
+  let address  = if port == 5000 {  format!( "127.0.0.1:{}", port) } else{  format!("0.0.0.0:{}", port) };
+
+  let listener = tokio::net::TcpListener::bind(address)
+  .await
+  .unwrap();
+
+  axum::serve(listener, app).await.unwrap();
+  // let sent_email = send_email().await;
 
   Ok(())
 }
